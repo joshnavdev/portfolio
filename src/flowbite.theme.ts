7dce556106da329d128724e4da2d7b6dae4df660
Joshua Navarro <joshua.navarro35@gmail.com>
2025-02-23 11:58:03 -0500

    add styling to responsive components

--- src/flowbite.theme.ts
+++ src/flowbite.theme.ts
@@ -1,7 +1,7 @@
 export const theme = {
   navbar: {
     toggle: {
-      base: 'inline-flex items-center rounded-lg p-2 text-sm text-nord-3 hover:bg-nord-5 focus:outline-none focus:ring-2 focus:ring-nord-5 dark:text-nord-6 dark:hover:bg-nord-3 dark:focus:ring-nord-3 md:hidden',
+      base: 'inline-flex items-center rounded-lg p-2 text-sm text-nord-6 hover:bg-nord-5 focus:outline-none focus:ring-2 focus:ring-nord-5 dark:text-nord-6 dark:hover:bg-nord-3 dark:focus:ring-nord-3 md:hidden',
     },
     root: {
       base: 'border-gray-200 px-2 py-2.5 dark:border-gray-700 bg-nord-0 sm:px-4',
@@ -12,7 +12,7 @@ export const theme = {
       },
     },
     collapse: {
-      list: 'mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 bg-white dark:bg-nord-1 md:bg-transparent dark:md:bg-transparent p-2 md-p-0 rounded-lg',
+      list: 'mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 bg-nord-3 md:bg-transparent dark:md:bg-transparent p-2 md-p-0 rounded-lg',
     },
   },
   button: {
